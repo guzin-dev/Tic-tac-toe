d34e76f1780de1906cce54a2584bd906c01569f6
guzin-dev <guzinhojk99@gmail.com>
2022-12-12 22:42:04 -0300

    Tile style bugado arrumado

--- src/components/TicTacToeTile.js
+++ src/components/TicTacToeTile.js
@@ -5,10 +5,10 @@ function TicTacToeTile(props) {
 
   return (
     <div
-      className={`w-32 h-32 border rounded-md transition-all flex justify-center items-center cursor-pointer hover:bg-slate-800 sm:w-16 h-16`}
+      className={`w-32 h-32 border rounded-md transition-all flex justify-center items-center cursor-pointer hover:bg-slate-800 sm:w-16 sm:h-16`}
       onClick={(e) => {
         e.target.className =
-          "w-32 h-32 border rounded-md transition-all flex justify-center items-center pointer-events-none sm:w-16 h-16";
+          "w-32 h-32 border rounded-md transition-all flex justify-center items-center pointer-events-none sm:w-16 sm:h-16";
         {
           props.selectorType == 0 ? setType("X") : setType("O");
         }
